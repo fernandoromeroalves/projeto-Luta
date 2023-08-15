01a3f97044a3d7a36b695621fc275660b88c8948
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-08-15 19:08:58 -0300

    fim

--- assets/js/script-funcional.js
+++ assets/js/script-funcional.js
@@ -1,3 +1,10 @@
 const char = createKnight('fernando romero');
 
 const monster = createLittleMonster();
+
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+);
